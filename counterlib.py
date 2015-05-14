7d71520fe582e1b900a066594414f708db7e71a9
Rejzor <209456@student.pwr.edu.pl>
2015-05-14 13:38:21 +0200

    Z countera do lotka. Lotek z wykorzytaniem counter-a

--- counterlib.py
+++ counterlib.py
@@ -6,8 +6,12 @@ counter = Counter()
 
 for lines in f:
     tab_lines = lines.split()  # note, no argument!
-    formatted_tab = map(int, tab_lines[-1].split(','))
+    formatted_tab = map(int, tab_lines[2].split(','))
     #print formatted_tab
     counter.update(i for i in formatted_tab if 0 < i < 50)
 
-print counter.most_common()
+print counter.items()
+
+sumall=sum(counter.values())     # Sunuhe wszystkie wartosci slownika
+for number, value in counter.items():  #Petla ktora wyswietla mi wszystie wylosowane numery wraz z liczba ile razy zostaly wylosowane oraz jaki to byl procent wszystkoich
+	print ('Number {} drawn {} times and it is {}/10000 of all ').format(number,value,10000*value/sumall)
